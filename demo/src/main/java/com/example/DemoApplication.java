9b1f5ea6755a87e541b95420e83ac0c08f08ea27
Javier <jmartin@grupoloyal.com>
2024-11-25 14:46:56 +0100

    Proyecto demo

---
+++ demo/src/main/java/com/example/DemoApplication.java
@@ -0,0 +1,29 @@
+package com.example;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.annotation.Order;
+
+@SpringBootApplication
+public class DemoApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.err.println("Aplicacion arrancada");
+	}
+
+	@Bean
+	@Order(1)
+	CommandLineRunner otro() {
+		return arg -> {
+			// System.err.println("Otro");
+			
+		};
+	}
+}
