5f9b6b94c80ed71f2c582356cf26062749610da1
Javier <jmartin@grupoloyal.com>
2024-11-25 17:01:38 +0100

    perfiles

--- demo/src/main/java/com/example/DemoApplication.java
+++ demo/src/main/java/com/example/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -18,10 +19,14 @@ public class DemoApplication implements CommandLineRunner {
 		System.err.println("Aplicacion arrancada");
 	}
 
+	@Value("${mi.valor:Valor por defecto}")
+	private String config;
+	
 	@Bean
 	@Order(1)
 	CommandLineRunner ioc() {
 		return arg -> {
+			System.err.println(config);
 //			Servicio srv = new ServicioImpl(new RepositorioImpl(new ConfiguracionImpl()));
 //			var contexto = new AnnotationConfigApplicationContext(AppConfig.class);
 ////			Servicio srv = contexto.getBean(Servicio.class);
