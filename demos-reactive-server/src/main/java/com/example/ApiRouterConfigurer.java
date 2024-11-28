2b4512e699a43cefb227ec9e253ee7c51db726d9
Javier <jmartin@grupoloyal.com>
2024-11-28 15:42:46 +0100

    Reactivos

---
+++ demos-reactive-server/src/main/java/com/example/ApiRouterConfigurer.java
@@ -0,0 +1,25 @@
+package com.example;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+import org.springframework.web.reactive.function.server.RequestPredicates;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+
+import com.example.resources.ActoresHandler;
+
+@Configuration
+@EnableWebFlux
+public class ApiRouterConfigurer implements WebFluxConfigurer {
+	public RouterFunction<?> routerPerson(ActoresHandler handler) {
+		return RouterFunctions.route().path("/actores/v2", ops -> ops
+				.GET("/{id}", RequestPredicates.accept(MediaType.APPLICATION_JSON), handler::getOne)
+				.GET(RequestPredicates.accept(MediaType.APPLICATION_JSON), handler::getAll)
+				.POST("", RequestPredicates.contentType(MediaType.APPLICATION_JSON), handler::create)
+				.PUT("/{id}", RequestPredicates.contentType(MediaType.APPLICATION_JSON), handler::update)
+				.DELETE("/{id}", handler::delete)
+				).build();
+	}
+}
